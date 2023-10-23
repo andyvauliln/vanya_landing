bc5888ce173fd3a307f51d0c894a1c1e3f24c5e8
Andrei Vaulin <andy.vaulin@gmail.com>
2023-10-24 07:14:49 +0800

    text-centered

--- src/components/testimonials2.jsx
+++ src/components/testimonials2.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 const TestimonialsMobile = () => {
   return (
     <div className="flex md:hidden z-20 w-full flex-col items-center justify-center bg-black p-8 ">
-      <div data-aos="zoom-in-right" data-aos-delay="0" className="p-4 ">
+      <div  className="p-4 text-center">
         <div className="gold-text2 font-extrabold">High Profile Client</div>
         <div className="bg-gradient-to-t from-gray-900 via-gray-300 to-gray-900 bg-clip-text text-transparent text-zinc-400">
           "When the odds were stacked against us, Pluto House delivered. They're
           the legal team you want in your corner when things get tough."
         </div>
       </div>
-      <div data-aos="zoom-in-right" data-aos-delay="0" className="p-4 ">
+      <div className="p-4 text-center">
         <div className="gold-text2 font-extrabold">High Profile Client</div>
         <div className="bg-gradient-to-t from-gray-900 via-gray-300 to-gray-900 bg-clip-text text-transparent text-zinc-400">
           "When the odds were stacked against us, Pluto House delivered. They're
           the legal team you want in your corner when things get tough."
         </div>
       </div>
-      <div data-aos="zoom-in-right" data-aos-delay="0" className="p-4 ">
+      <div  className="p-4 text-center">
         <div className="gold-text2 font-extrabold">High Profile Client</div>
         <div className="bg-gradient-to-t from-gray-900 via-gray-300 to-gray-900 bg-clip-text text-transparent text-zinc-400">
           "When the odds were stacked against us, Pluto House delivered. They're
